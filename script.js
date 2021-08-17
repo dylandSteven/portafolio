181d262655dbbb1d363e511549793a454ac80ad5
dylandSteven <stevendylandsaldana@gmail.com>
2021-08-17 00:36:22 -0500

    YaCasi

--- script.js
+++ script.js
@@ -1,6 +1,23 @@
-
-
 var el = $('.js-tilt-container');
+var navbar =$('.navbar')
+
+$(window).scroll(function()
+{
+	if($(window).scrollTop()>700)
+	{
+
+		navbar.removeClass("navbar-3")
+		navbar.addClass("navbar-2")
+
+		
+	}else
+	{
+		navbar.removeClass("navbar-2")
+		navbar.addClass("navbar-3")
+	}
+})
+
+
 
 el.on('mousemove', function(e){
 	const {left, top} = $(this).offset();
@@ -10,13 +27,12 @@ el.on('mousemove', function(e){
 	const cursFromCenterY = $(this).height() / 2 - cursPosY;
 	
 
-	$(this).css('transform','perspective(500px) rotateX('+ (cursFromCenterY / 40) +'deg) rotateY('+ -(cursFromCenterX / 40) +'deg) translateZ(10px)');
+$(this).css('transform','perspective(500px) rotateX('+ (cursFromCenterY / 40) +'deg) rotateY('+ -(cursFromCenterX / 40) +'deg) translateZ(10px)');
     
-    console.log("2")
-  const invertedX = Math.sign(cursFromCenterX) > 0 ? -Math.abs( cursFromCenterX ) : Math.abs( cursFromCenterX );
-  
+const invertedX = Math.sign(cursFromCenterX) > 0 ? -Math.abs( cursFromCenterX ) : Math.abs( cursFromCenterX );
+
   //Parallax transform on image
-  $(this).find('.js-perspective-neg').css('transform','translateY('+ ( cursFromCenterY / 10) +'px) translateX('+ -(invertedX  / 10) +'px) scale(1.15)');
+$(this).find('.js-perspective-neg').css('transform','translateY('+ ( cursFromCenterY / 10) +'px) translateX('+ -(invertedX  / 10) +'px) scale(1.15)');
 
 	$(this).removeClass('leave');
 });
@@ -24,7 +40,16 @@ el.on('mousemove', function(e){
 el.on('mouseleave', function(){
 	$(this).addClass('leave');
     
-    console.log("3")
 });
 
+/* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
+/* ---- particles.js config ---- */
 
+
+  /* ---- stats.js config ---- */
+  
+/* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
+particlesJS.load('particles-js', 'assets/particles.json', function() {
+	console.log('callback - particles.js config loaded');
+  });
+  
